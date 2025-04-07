ba43028e764f98853e7d6deaaf6dc12663109911
Ramses C. Labora <ramzaa941@gmail.com>
2025-04-08 00:34:32 +1100

    Homepage(Initial)
    Fixed the directory files, made the initial design of the homepage.

--- src/main/java/com/arise/petify/controller/PetController.java
+++ src/main/java/com/arise/petify/controller/PetController.java
@@ -7,8 +7,30 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class PetController {
+  
+    @GetMapping("/about")
+    public String aboutPage() {
+        return "about"; // Looks for about.html inside src/main/resources/templates/
+    }
+    
+    @GetMapping("/services")
+    public String servicePage() {
+        return "services"; // Looks for about.html inside src/main/resources/templates/
+    }
+
+
+    @GetMapping("/contacts")
+    public String contactPage() {
+        return "contacts"; // Looks for about.html inside src/main/resources/templates/
+    }
+
 
     @GetMapping("/")
+    public String homePage() {
+        return "Homepage"; // This points to Homepage.html inside /templates
+    }
+
+    @GetMapping("/profile")
     public String profile() {
         return "profile"; // this will look for profile.html inside templates/
     }
