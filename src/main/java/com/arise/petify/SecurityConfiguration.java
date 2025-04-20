f70c44a6d9c74432e99db46b31352f5e6393bad6
Ramses C. Labora <ramzaa941@gmail.com>
2025-04-20 15:59:01 +1100

    Ram[Updated-Feat.]
    Updated the Prof.

--- src/main/java/com/arise/petify/SecurityConfiguration.java
+++ src/main/java/com/arise/petify/SecurityConfiguration.java
@@ -13,7 +13,7 @@ public class SecurityConfiguration {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/", "/login", "/register", "/homepage", "/api/**", "/images/**", "/config/**").permitAll()
+                .requestMatchers("/", "/login", "/register", "/homepage", "/api/**", "/images/**", "/config/**","/homepage/**","/profile/**","/overview/**","/inbox/**").permitAll()
                 .anyRequest().authenticated()
             )
             .formLogin(form -> form.disable());
